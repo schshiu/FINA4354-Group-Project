a6a8e04e8062d77f9416f5deaacc104346d305f7
MightySimon <71126604+MightySimon@users.noreply.github.com>
2021-04-14 22:32:12 +0800

    Update FINA 4354 Project Code.R

--- FINA 4354 Project Code.R
+++ FINA 4354 Project Code.R
@@ -1,64 +1,73 @@
 #FINA 4354 Project
 
-# 1- Downloading data
+# 1. Preparation settings
 library("xts")
 library("quantmod")
-Und.price.raw <- getSymbols("SPY", from = "2018-01-01", auto.assign = FALSE)
-# if possible I want data from "past X years", I forgot, is this possible?! 
-# e.g. Today is 7 Apr, so I want data from 2018-04-07
-# i found that HSI has very high outliers of returns, up to 10^5, , dont know why
-Und.price <- na.locf(Und.price.raw, na.rm = FALSE)      #fill up the NA values
-# i think na.locf function may cause some errors, but na.approx does not apply to
-# the last terms
-head(Und.price, 3)
-cat("------------------------------------------------------------\n")
-tail(Und.price, 3)     #inspect data
+library("lubridate")
+setwd("/Users/fuxipeng/1/港大学习资料/FINA4354/FINA 4354 GP")
+# the working directory has to be set before running on other computers
 
-# 1.2 find risk-free rate
-# we use FRED 1-Year Treasury Constant Maturity Rate (DGS1) as proxy
+#-------------------------------------------------------------------------------
+
+# 2. Download data and initial analysis
+# 2.1. Download underlying data
+
+# we have downloaded SPY for the last 10 years, 
+# but only the last 3 years are for simulation purposes
+Und.price.raw <- getSymbols("SPY", from = Sys.Date() - years(x = 10), 
+                            auto.assign = FALSE)
+
+# Use graph to show trend
+barChart(Und.price.raw, theme = "white.mono", bar.type = "hlc")
+# A general upward trend, but there are few but large falls
+
+# filtering out needed data
+Und.price <- na.approx(window(Und.price.raw, start = Sys.Date() - years(x = 3)), 
+                       na.rm = FALSE)
+# na.approx will reduce errors
+# na.locf will fill the gap at the end
+Und.return <- coredata(dailyReturn(Und.price))  #this is a vector
+
+# QQ plot analysis
+qqnorm(Und.return)
+# we can see the return is not strictly normally distributed
+# there are LHS -ve and RHS +ve outliers, and the main line is not straight
+
+# 2.2 find risk-free rate
+# we use FRED 6-month Treasury Constant Maturity Rate (DGS6MO) as proxy
 # this is dynamic, so we use downloaded data instead of pre-set data
-RFrate.raw <- getSymbols("DGS1", from = "2020-01-01", src = "FRED", auto.assign = FALSE)
+RFrate.raw <- getSymbols("DGS6MO", from = Sys.Date() - months(x = 1), src = "FRED", auto.assign = FALSE)
 # this "from" doesn't work for no reason
-RF.rate <- 0.06 # change later
+RF.rate <- mean(last(RFrate.raw, 10))
+# we use the average of last 10 obs
 
 #-------------------------------------------------------------------------------
 
-# 2- Storing data to local repository
+# 2- Storing data to, and loading data from local repository
 # determines the saving filename and directory
-saveRDS(Und.price, file = "/Users/fuxipeng/1/港大学习资料/FINA4354/FINA 4354 GP/data/Und_Prices_Processed")
-# !!! This file path has to be altered
-# i dont know how to write dependent paths
+saveRDS(Und.price, file = "/data/Und_Prices_Processed")
+# is this dependent path after I set working directory?
 
-#-------------------------------------------------------------------------------
-
-# 3- Loading data from local repository
-Und.price <- readRDS("/Users/fuxipeng/1/港大学习资料/FINA4354/FINA 4354 GP/data/Und_Prices_Processed")
-# this save file is not a tabular file, very wierd
+Und.price <- readRDS("/data/Und_Prices_Processed")
+# this save file is not a tabular file
 # different from traditional read.csv command
 
 #-------------------------------------------------------------------------------
 
-# 4- Financial analysis
-# 4.1 find returns
-Und.return <- coredata(dailyReturn(Und.price))  #this is a vector
-head(Und.return, 3)
-cat("------------------------------------------------------------\n")
-tail(Und.return, 3)     #inspect data
+# 3- Financial analysis
+# 3.2 find parameters miu and sigma
+Und.r.miu <- mean(Und.return)
+Und.r.sigma <- sd(Und.return) / sqrt(1/252)  # annualized
 
-# 4.2 find parameters miu and sigma
-Und.ret.miu <- mean(Und.return)
-Und.ret.sigma <- sd(Und.return) / sqrt(1/252)  # annualized
-
-# 4.3 set parameters
+# 3.3 set parameters
 n <- nrow(Und.price)
 S0 <- coredata(Und.price$SPY.Adjusted[n])   #the last day's adjusted price
 T <- 1  # time till maturity
 K <- S0 # at the money call option
 r <- 0  # where can i find risk free interest rate???
 
-# 4.4 For European call option:
+# 3.4 For European call option:
 # price is determined by Black-Scholes Formula
 d1 <- (log(S0 / K) + (r + 0.5 * sigma^2) * T) / (sigma * sqrt(T))
 d2 <- d1 - sigma * sqrt(T)
 (C0 <- pnorm(d1) * S0 - pnorm(d2) * K * exp(-RF.rate * T))
-
